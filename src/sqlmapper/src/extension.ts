57bb7315facd0ddd03f2b1aac37eaba9b730cace
David Sharp <David.M.Sharp@WellsFargo.com>
2017-06-23 16:16:45 -0400

    working on sql

--- src/sqlmapper/src/extension.ts
+++ src/sqlmapper/src/extension.ts
@@ -9,6 +9,9 @@ import * as http from 'http';
 import HttpsProxyAgent = require('https-proxy-agent');
 import HttpProxyAgent = require('http-proxy-agent');
 import { parse as parseUrl } from 'url';
+import { SqlService } from "./services/sqlService"
+
+let controller: any = undefined;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,46 +21,37 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "sqlmapper" is now active!');
 
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.order66', () => {
-        const message = "All the jedi are now dead...";
-        vscode.window.showInformationMessage(message);
+        var sqlService = new SqlService();
+        sqlService.get();
 
-        const fullUrl = "https://raw.githubusercontent.com/seesharper/Dotnet.Script.NuGetMetadataResolver/master/build/project.json";
-        const proxyUrl = vscode.workspace.getConfiguration("http").get("proxy").toString();
-        const proxyUrlConst = "http://proxy.wellsfargo.com:8080";
-        var compare = proxyUrl === proxyUrlConst;
-        // var proxyPort = 8080;
-        const testUrl = "http://domain.com:3000/path/to/something?query=string#fragment";
-        var parsedTestUrl = parseUrl(testUrl);
-        var parsedProxyUrl = parseUrl(proxyUrlConst);
-        var proxyOptions = {
-            host: parsedProxyUrl.hostname,
-            port: Number(parsedProxyUrl.port),
-            auth: parsedProxyUrl.auth,
-            rejectUnauthorized: false
-        }
+        //     var pick = vscode.window.showQuickPick(["1", "2", "3"]);
+        //     pick.then(v => {
+        //         console.log(v);
+        //     });
 
-        var agent = new HttpProxyAgent(proxyOptions);
-        var parsedUrl = parseUrl(fullUrl);
-        var requestOptions: https.RequestOptions = {
-            host: parsedUrl.host,
-            path: parsedUrl.path,
-            agent: agent,
-            rejectUnauthorized: false
-        };
+        //     const fullUrl = "https://raw.githubusercontent.com/seesharper/Dotnet.Script.NuGetMetadataResolver/master/build/project.json";
+        //     const proxyUrl = "http://proxy.wellsfargo.com:8080";
 
-        console.log("initiating call");
-        http.request({ host: "www.google.com", agent: agent }, response => {
-            console.log("response received");
-            console.log(response);
-        })
-        // https.request(requestOptions, response => {
-        //     console.log("response received");
-        //     console.log(response);
-        // });
+        //     var agent = new HttpsProxyAgent(proxyUrl);
+
+        //     var parsedUrl: any = parseUrl(fullUrl);
+        //     var requestOptions: https.RequestOptions = {
+        //         host: parsedUrl.host,
+        //         path: parsedUrl.path,
+        //         agent: agent,
+        //         rejectUnauthorized: false
+        //     };
+
+        //     console.log("initiating call");
+        //     https.get(requestOptions, response => {
+        //         console.log("response received");
+        //         response.pipe(process.stdout);
+        //     });
     });
 
     context.subscriptions.push(disposable);

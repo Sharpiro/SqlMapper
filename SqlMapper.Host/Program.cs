8d1851529d45a4e7b42561e1e4ca739777ce999e
Sharpiro <dsharpbb09@gmail.com>
2017-06-26 10:33:12 -0400

    server access working

--- SqlMapper.Host/Program.cs
+++ SqlMapper.Host/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SqlMapper.Host
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cmdApp = new CommandLineApplication(throwOnUnexpectedArg: false);
+            cmdApp.HelpOption("-? | -h | --help");
+
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(new[] { "--server.urls", "http://localhost:2000" });
+
+            var builder = new WebHostBuilder()
+                .UseConfiguration(config.Build())
+                .UseEnvironment("SqlMapper")
+                .ConfigureServices(serviceCollection =>
+                {
+                })
+                .UseStartup(typeof(Startup))
+                .UseKestrel();
+
+            cmdApp.OnExecute(() =>
+            {
+                using (var webApp = builder.Build())
+                {
+                    webApp.Start();
+                    var appLifeTime = webApp.Services.GetRequiredService<IApplicationLifetime>();
+                    appLifeTime.ApplicationStopping.WaitHandle.WaitOne();
+                }
+
+                return 0;
+            });
+
+            cmdApp.Execute();
         }
     }
 }
---
+++ SqlMapper.Host/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SqlMapper.Host
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider, ILoggerFactory loggerFactory)
+        {
+
+        }
+    }
+}

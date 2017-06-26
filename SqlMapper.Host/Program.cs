7122dd2dfe428c8f01b7d60b41258b979e7587bb
Sharpiro <dsharpbb09@gmail.com>
2017-06-26 09:51:22 -0400

    adding web server host project

---
+++ SqlMapper.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlMapper.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

fca925efd293296cad913ea2a637cc346df29989
Sharpiro <dsharpbb09@gmail.com>
2017-06-26 11:08:27 -0400

    upgraded scaffolding project

--- SqlMapper.Host/Api/TestController.cs
+++ SqlMapper.Host/Api/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Scaffolding;
 
 namespace SqlMapper.Host.Api
 {
@@ -7,8 +8,8 @@ namespace SqlMapper.Host.Api
         public async Task<object> Get(string connectionString, string databaseName)
         {
             await Task.Yield();
-            //var scaffolder = new Scaffolder();
-            //var scaffolding = scaffolder.ScaffoldDatabase(connectionString, "GeneratedNamespace", $"{databaseName}Context");
+            var scaffolder = new Scaffolder();
+            var scaffolding = scaffolder.ScaffoldDatabase(connectionString, "GeneratedNamespace", $"{databaseName}Context");
             //var sourceBuilder = new SourceBuilder();
             //sourceBuilder.Build(scaffolding.AllFiles);
             return new { Data = "testData" };
